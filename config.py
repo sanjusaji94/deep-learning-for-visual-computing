bec96805976e4a9f2571f2f05abfaf12176cb2f7
sanjusaji94 <sanjusaji94@gmail.com>
2026-05-03 11:57:14 +0200

    configuration setup

--- config.py
+++ config.py
@@ -1,16 +1,22 @@
 """
 Central configuration for machine-dependent paths used in assignment scripts.
 
-Students should adapt these paths to their local setup.
 """
 
 from pathlib import Path
 
+# To find the location of the project folder moves
+ROOT_DIR = Path(__file__).resolve().parent
+
 # Path to extracted CIFAR-10 python files (directory containing data_batch_1 ... test_batch)
-DATA_DIR = Path("/path/to/cifar-10-batches-py")
+DATA_DIR = ROOT_DIR / "datasets"/ "cifar-10-batches-py"
 
 # Optional logging directory (for wandb/tensorboard/custom logs)
-LOG_DIR = Path("logs")
+LOG_DIR = ROOT_DIR / "logs"
 
 # Directory where trained model checkpoints are stored
-MODEL_SAVE_DIR = Path("saved_models")
+MODEL_SAVE_DIR = ROOT_DIR / "saved_models"
+
+# Create folders automatically if they do not exist.
+LOG_DIR.mkdir(exist_ok=True)
+MODEL_SAVE_DIR.mkdir(exist_ok=True)
